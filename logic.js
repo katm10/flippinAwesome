2223e5ccd666ed019b3eef1c19398580e6341536
Katherine Mohr <katm10@live.com>
2019-09-14 18:55:59 -0400

    fixed push bug

--- logic.js
+++ logic.js
@@ -16,7 +16,7 @@ function fillTiles(numFlip, textArr) {
 
 function playRound(arr) {
   numFlip = 4;
-  fillTiles(numFlip, textArr);
+  fillTiles(numFlip, arr);
   $(".grid-cell").css("background", "#cbf7f4");
 
   var currentTile = 0;
@@ -39,18 +39,21 @@ function startGame() {
   text = $(".word-container")
     .find("textarea")
     .val();
-  console.log(text);
   const textArr = text.split(" ");
-  var arrCounter = -1;
+  var arrCounter = 0;
   var arrOfArrs = Array(Math.ceil(textArr.length / 16));
+  arrOfArrs[0] = [];
+
   for (var i = 0; i < textArr.length; i += 1) {
-    if (i % 16 == 0) {
+    if (i % 16 == 0 && i !=0) {
       arrCounter += 1;
+      arrOfArrs[arrCounter] = []
     }
     arrOfArrs[arrCounter].push(textArr[i]);
   }
   
   for(var i = 0; i < arrOfArrs.length; i++){
+      console.log(arrOfArrs[i])
       playRound(arrOfArrs[i])
   }
 
