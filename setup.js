ff842592182f746d96cbfb431392d0d1eb74f8da
John Sanchez <johnlsanchez@gmail.com>
2019-09-14 18:37:15 -0400

    update ui

--- setup.js
+++ setup.js
@@ -2,6 +2,8 @@
 var height = 4;
 var width = 4;
 
+var hiddenText = "";
+
 
 for (var i = 0; i < height; i++) {
 	$(".grid-container").append("<div class='grid-row'></div>");
@@ -9,3 +11,21 @@ for (var i = 0; i < height; i++) {
 		$(".grid-container .grid-row:last-child").append("<div class='grid-cell'></div>")
 	}
 }
+
+$(".hide-show").click(function() {
+	var textarea = $("textarea");
+
+	if (textarea.attr('disabled')) {
+		textarea.attr('disabled', false);
+		textarea.val(hiddenText);
+		textarea.attr("placeholder", "Type the words you want to memorize in here...");
+	} else {
+		textarea.attr('disabled', "disabled");
+		hiddenText = textarea.val();
+		textarea.attr("placeholder", "(text currently being hidden - click the button below to see again)");
+		textarea.val("");
+	}
+
+
+
+});
